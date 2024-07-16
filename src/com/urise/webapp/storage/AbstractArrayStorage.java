db30d2987f51a57d19750d7de81bb777622dfeef
Kvitto <kian04@mail.ru>
2024-07-17 00:40:25 +0300

    Lesson03 abstract storage & sorted storage

---
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -0,0 +1,24 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+public abstract class AbstractArrayStorage implements Storage {
+    protected static final int STORAGE_LIMIT = 10000;
+    protected final Resume[] storage = new Resume[STORAGE_LIMIT];
+    protected int size;
+
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " not exist");
+            return null;
+        }
+        return storage[index];
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    protected abstract int getIndex(String uuid);
+}
