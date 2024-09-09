7b2a235f0df8f17b0491614f52632027e584c872
Kvitto <kian04@mail.ru>
2024-09-09 17:59:26 +0300

    HW12 Optional

--- src/com/urise/webapp/MainStream.java
+++ src/com/urise/webapp/MainStream.java
@@ -2,20 +2,23 @@ package com.urise.webapp;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MainStream {
     public static void main(String[] args) {
         System.out.println(minValue(new int[]{1, 2, 3, 3, 2, 3}));
         System.out.println(minValue(new int[]{9,8}));
-        System.out.println(oddOrEven(List.of(new Integer[]{1, 2, 3, 3, 2, 3, 5})));
+        System.out.println(oddOrEven(List.of(new Integer[]{1, 2, 3, 3, 1, 3, 5})));
     }
 
     private static int minValue(int[] values) {
-        return Arrays.stream(values).distinct().sorted().reduce((acc, x) -> acc * 10 + x).getAsInt();
+        return Arrays.stream(values).distinct().sorted().reduce(0, (acc, x) -> acc * 10 + x);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        Integer sum = integers.stream().reduce(0, Integer::sum);
-        return integers.stream().filter(a -> sum % 2 > 0 ? a % 2 < 1 : a % 2 > 0).toList();
+        Map<Integer, List<Integer>> mapResult = integers.stream()
+                .collect(Collectors.groupingBy(x -> x % 2, Collectors.toList()));
+        return mapResult.get(1).size() % 2 == 0 ? mapResult.get(1) : mapResult.get(0);
     }
 }
