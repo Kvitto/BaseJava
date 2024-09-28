58bfc710e1dc3aee3cba578c2b2c2b22476f5196
Kvitto <kian04@mail.ru>
2024-09-28 20:20:57 +0300

    HW15 ResumeServlet
    - added connection to db.resumes
    - response all resumes to web table

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -1,11 +1,16 @@
 package com.urise.webapp.web;
 
+import com.urise.webapp.Config;
+import com.urise.webapp.model.Resume;
+import com.urise.webapp.storage.Storage;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
     @Override
@@ -13,8 +18,27 @@ public class ResumeServlet extends HttpServlet {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=UTF-8");
-        String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello Senior Java Developer" : "Hello " + name + " - new Senior Java Developer");
+
+        Storage db = Config.get().getStorage();
+        List<Resume> resumes = db.getAllSorted();
+
+        PrintWriter writer = response.getWriter();
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("<html><head><title>Resume Servlet</title></head>");
+        sb.append("<body>");
+        sb.append("<h1>Resume Servlet</h1>");
+        sb.append("<table border=1>");
+        resumes.forEach(resume -> {
+            sb.append("<tr>")
+                    .append("<td>").append(resume.getUuid()).append("</td>")
+                    .append("<td>").append(resume.getFullName()).append("</td>")
+                    .append("</tr>");
+        });
+        sb.append("</table>");
+        sb.append("</body></html>");
+
+        writer.append(sb.toString());
     }
 
     @Override
